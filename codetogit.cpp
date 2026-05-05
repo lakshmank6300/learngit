94e8518df01b6330be87aafbc9e23fe8ea3f0262
Lakshman Kamparapu <kamparapu.lakshman.37.3@mountblue.tech>
2026-05-05 20:00:57 +0530

    thisi coming

--- codetogit.cpp
+++ codetogit.cpp
@@ -8,7 +8,6 @@ using namespace std;
 int main(){
     int n,c;
     cin>>n;
-    vector<vector<int>>arr;
     for(int i=0;i<n;i++){
         vector<int>mini;
         for(int i=0;i<n;i++){
@@ -18,6 +17,7 @@ int main(){
         arr.push_back(mini);
     }
     int res=0;
+    arr[i] =1;
     for(int i=0;i<n;i++){
         res+=arr[i][i];
         res+=arr[i][n-i-1];
